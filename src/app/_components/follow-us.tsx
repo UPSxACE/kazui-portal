4b9eef370ee7dac18120d9733dde31b4400f3919
Eduardo Botelho <eduardo11224b@gmail.com>
2024-12-01 16:24:43 +0000

    follow us

---
+++ src/app/_components/follow-us.tsx
@@ -0,0 +1,40 @@
+import Dexscreener from "@/components/icons/dexscreener";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaInstagram, FaXTwitter } from "react-icons/fa6";
+
+export default function FollowUs() {
+  return (
+    <div className="w-full bg-background rounded-md overflow-hidden shrink-0">
+      <h2 className="text-font-1 font-semibold text-lg px-3 py-2 bg-background-lighter">
+        Follow Us
+      </h2>
+      <div className="flex gap-3 px-3 py-3 justify-evenly">
+        <IconLink href="https://twitter.com/Kazui_Club">
+          <FaXTwitter />
+        </IconLink>
+        <IconLink href="https://dexscreener.com/solana/cqlhcuudf5gncuhp3mnpnhzfwugpemuwhztnsmcdf4t">
+          <Dexscreener />
+        </IconLink>
+        <IconLink href="https://t.me/Kazui_club">
+          <FaTelegramPlane className="pr-[0.15rem]" />
+        </IconLink>
+        <IconLink href="https://www.instagram.com/kazui.club">
+          <FaInstagram />
+        </IconLink>
+      </div>
+    </div>
+  );
+}
+
+function IconLink({ children, href }: { children: ReactNode; href: string }) {
+  return (
+    <Link
+      href={href}
+      className="text-font-1 hover:text-white border-2 border-font-1 hover:border-white border-solid rounded-full h-9 w-9 text-xl flex justify-center items-center"
+    >
+      {children}
+    </Link>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Events from "./_components/events";
+import FollowUs from "./_components/follow-us";
 import NewAccounts from "./_components/new-accounts";
 import Posts from "./_components/posts";
 import Richest from "./_components/richest";
@@ -10,6 +11,7 @@ export default function Page() {
       <aside className="basis-[20rem] py-4 hidden 2md:flex 2xl:flex flex-col gap-4 order-1 2xl:order-none overflow-y-auto hide-scroll">
         <Events />
         <Richest />
+        <FollowUs />
       </aside>
       <Posts />
       <aside className="basis-[20rem] py-4 hidden 2xl:flex flex-col gap-4 overflow-y-auto hide-scroll">
